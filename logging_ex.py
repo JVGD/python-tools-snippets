ef8c08547cf31a419cf1704169dbe29e17a264f7
Javier V <j.vargas.g.d@gmail.com>
2018-09-20 14:19:27 +0200

    Logging to rotating file and to stdout

--- logging_ex.py
+++ logging_ex.py
@@ -1,30 +1,43 @@
 import logging
+import sys
 from logging.handlers import RotatingFileHandler
 
-# Handler conf
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = RotatingFileHandler('carrier_watchdog.log', maxBytes=2000, backupCount=10)
-handler.setFormatter(formatter)
-handler.setLevel(logging.DEBUG)
+# General formater for logs in here we define the logs format
+formatter = logging.Formatter('%(asctime)s %(name)s [%(levelname)s] %(message)s')
+
+# Handler for logging to a file
+file_handler = RotatingFileHandler('carrier_watchdog.log', maxBytes=2000, backupCount=10)
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+# Handler for logging to stdout
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
 
 # You can define different names for different
 # loggers within the app, ideally you can call
 # a logger per file with name __file__ and then
-# use logger at every place
+# use logger at every place, you can also have
+# different handlers within one logger
 logger = logging.getLogger(__file__)
+# Set global level of logging
 logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+# Adding handlers
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
 
 if __name__ == "__main__":
 
 	try:
-		for n in range(200):
+		for n in range(1000):
 			logger.debug(str(n) + ' debug message')
 			logger.info(str(n) + ' info message')
 			logger.warn(str(n) + ' warn message')
 			logger.error(str(n) + ' error message')
 			logger.critical(str(n) + ' critical message')
-			if n == 100:
+			if n == 900:
 				raise IOError("This is an error")
 	
 	except Exception as e:
