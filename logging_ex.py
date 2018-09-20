b571d427f421d6fd0fbe54485534e784938017d7
Javier V <j.vargas.g.d@gmail.com>
2018-09-20 19:07:40 +0200

    Propagate false for different loggers

--- logging_ex.py
+++ logging_ex.py
@@ -5,27 +5,22 @@ from logging.handlers import RotatingFileHandler
 # General formater for logs in here we define the logs format
 formatter = logging.Formatter('%(asctime)s %(name)s [%(levelname)s] %(message)s')
 
+# Handler for logging to stdout
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+
 # Handler for logging to a file
 file_handler = RotatingFileHandler('carrier_watchdog.log', maxBytes=2000, backupCount=10)
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
 
-# Handler for logging to stdout
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-console_handler.setLevel(logging.DEBUG)
-
-# You can define different names for different
-# loggers within the app, ideally you can call
-# a logger per file with name __file__ and then
-# use logger at every place, you can also have
-# different handlers within one logger
+# Getting the logger that will handle multiple handlers
 logger = logging.getLogger(__file__)
-# Set global level of logging
 logger.setLevel(logging.DEBUG)
-# Adding handlers
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
+logger.propagate = False
 
 
 if __name__ == "__main__":
@@ -38,7 +33,8 @@ if __name__ == "__main__":
 			logger.error(str(n) + ' error message')
 			logger.critical(str(n) + ' critical message')
 			if n == 900:
-				raise IOError("This is an error")
+				# Forcing an error
+				raise IOError("This is a forced error")
 	
 	except Exception as e:
 		# Log and dumps error info
