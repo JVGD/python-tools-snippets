377bd7679d5f0c6255e3c0aa57c4feafa3566abe
Javier V <j.vargas.g.d@gmail.com>
2019-02-21 23:20:53 +0100

    Looping through dict path working

--- get_dict_like_paths.py
+++ get_dict_like_paths.py
@@ -1,4 +1,7 @@
-def get_dict_paths(dictionary, path="", path_list=[]):
+import json
+
+
+def get_dict_paths_verbose(dictionary, path="", path_list=[]):
     """Loops over a dictionary tree
     
     Parameters
@@ -15,21 +18,91 @@ def get_dict_paths(dictionary, path="", path_list=[]):
 
     for (key, value) in dictionary.items():
         
-        path = path + "/" + key
-        # path = path_conc(path, key)
-
-        # print("%s" % path, end="")
+        # path = path + "/" +  key
+        path_list.append(key) 
+        # print(path_list)
+        print("path:---------------> " + "/".join(path_list))
+        # print(path)
 
         # If value is a dict
         if isinstance(value, dict):
-            get_dict_paths(value, path=path, path_list=path_list)
-
-        # If value is a value
-        elif isinstance(value, list):
-            for element in value:
-                get_dict_paths(element, path=path, path_list=path_list)
+            print("down: append")
+            get_dict_paths(value, path=path, path_list=path_list)    
+            try:
+                path_list.pop()
+            except Exception as e:
+                pass
+            print("up  : pop")
+            
+        # if value print it 
         else:
-            print(path+value)
-            path_list.append(path+value)
+            try:
+                path_list.pop()
+            except Exception as e:
+                pass
+            # # path_list.append(value)
+            # print("/".join(path_list))
+            pass
     
-    return path_list
+    return path
+
+
+def get_dict_paths(dictionary, ret_list=[], current_path=[]):
+    """Loops over a dictionary tree
+    
+    Parameters
+    ----------
+    dictionary : dict
+        dict to iterate over
+
+    Returns
+    -------
+    paths_list : list
+        List with dict path for each element in dict
+    
+    """
+
+    for (key, value) in dictionary.items():
+        
+        # Keeping track of current path in tree
+        current_path.append(key) 
+
+        # If value is a dict we iterate again
+        if isinstance(value, dict):
+            # Go down one level
+            get_dict_paths(value, ret_list=ret_list, current_path=current_path)
+            
+            # Go up and popping element from current path
+            current_path.pop()
+            
+        # If value is not a dict, just a value
+        # TODO: handle lists
+        else:
+            # Reached final element
+            # Getting unix like path
+            path = "/".join(current_path) + "/" + value
+
+            # Appending path to output list
+            ret_list.append(path)
+
+            # Printing
+            print(path)
+
+            # Popping element from path
+            current_path.pop()
+    
+    return ret_list
+
+
+def read_json(path_to_file):
+    with open(path_to_file) as f:
+        data = json.load(f)
+    
+    return data
+
+
+conf = read_json("dir.json")
+paths = get_dict_paths(conf, ret_list=[], current_path=[])
+
+print("Path lists")
+print(paths)
